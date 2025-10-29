4d19d612851f1e0d3a860aba52372accc971b499
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-10-29 18:27:57 -0400

    Added Dealership class and added getters/setters for vehicle class

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+}
